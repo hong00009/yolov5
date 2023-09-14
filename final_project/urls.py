6c96adce049ffd79d7ae3e317bc8da519f5b0293
junhyukM <minjh1031@gmail.com>
2023-09-14 16:07:42 +0900

    first commit

--- final_project/urls.py
+++ final_project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from yolov5_django import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index), 
+    path('yolov5_django/', include('yolov5_django.urls')), 
 ]
