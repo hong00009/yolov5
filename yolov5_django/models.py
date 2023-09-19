c23929b386290c4f99126220278c740ef5cb84c6
hong <itsmezarya@gmail.com>
2023-09-19 16:07:11 +0900

    영양정보db

--- yolov5_django/models.py
+++ yolov5_django/models.py
@@ -9,3 +9,13 @@ class UploadedImage(models.Model):
     def __str__(self):
         return self.title
 
+class FoodNutrition(models.Model):
+    food_name = models.CharField(max_length=255)
+    calories = models.FloatField()
+    carbohydrates = models.FloatField()
+    fat = models.FloatField()
+    protein = models.FloatField()
+    # 다른 필요한 필드 추가 가능
+
+    def __str__(self):
+        return self.food_name
