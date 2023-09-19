4027538f9bc9ab287d72ab5a143f6336387abf6b
hong <itsmezarya@gmail.com>
2023-09-19 14:35:23 +0900

    index수정, accounts 앱시작

---
+++ accounts/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import CustomUserCreationForm, CustomAuthenticationForm, CustomUserChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if  form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('yolov5_django:index')
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'accounts/form.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('yolov5_django:index')
+        
+    else:
+        form = CustomAuthenticationForm()
+
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'accounts/form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
+
+
+# def mypage(request):
+# return render(request, 'accounts/mypage.html', context)
