fdd818fc69b9d9c196692fef45ccbc18f932616e
junhyukM <minjh1031@gmail.com>
2023-09-20 14:16:53 +0900

    detail페이지 추가

--- yolov5_django/urls.py
+++ yolov5_django/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 
     path('edit_image/<int:image_id>/', views.edit_image, name='edit_image'),
     path('delete_image/<int:image_id>/', views.delete_image, name='delete_image'),
+    path('image/<int:image_id>/', views.detail_image, name='detail_image'),
 ]
