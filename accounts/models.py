3ced23582b60423195df8e7aae574f9d9624ef76
hong <itsmezarya@gmail.com>
2023-09-22 13:07:17 +0900

    사용자 신체정보 / 사용자 섭취음식정보 model작성
    Signed-off-by: hong <itsmezarya@gmail.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    gender = models.CharField(max_length=10)  # 성별 (남성, 여성, 기타 등)
+    birthdate = models.DateField()  # 생년월일
+    height = models.FloatField()  # 키 (예: 175.5 cm)
+    weight = models.FloatField()  # 현재 몸무게 (예: 70.5 kg)
+
+    def calculate_bmi(self):
+        height_in_meters = self.height / 100.0
+        bmi = self.weight / (height_in_meters * height_in_meters)
+        return bmi
+    
+    def __str__(self):
+        return f"{self.user.username}님 프로필"
+    
+
+class UserFoodNutritions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # User 모델과 연결
+    
+    energy = models.FloatField() # 열량
+    carbohydrate = models.FloatField() # 탄수화물
+    protein = models.FloatField() # 단백질
+    fat = models.FloatField() # 지방
+    sodium = models.FloatField() # 나트륨
+    datetime = models.DateTimeField(default=timezone.now) # 
+
+    def __str__(self):
+        return f"{self.user.username}님의 섭취영양정보"
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 
@@ -46,5 +48,40 @@ def logout(request):
     return redirect('yolov5_django:index')
 
 
-# def mypage(request):
-# return render(request, 'accounts/mypage.html', context)
+# BMI 계산
+def calculate_bmi(height, weight):
+    # 키를 미터로 변환
+    height_in_meters = height / 100.0
+    
+    # BMI 계산 (체중(kg) / (키(m) * 키(m)))
+    bmi = weight / (height_in_meters * height_in_meters)
+    
+    return bmi
+
+from django.shortcuts import get_object_or_404
+
+@login_required
+def profile(request):
+    user = request.user
+    try:
+        profile = UserProfile.objects.get(user=user)
+        if not profile.profile_completed:
+            # 프로필 정보를 입력하지 않은 경우 프로필 입력 페이지로 리디렉션
+            return redirect('accounts:profile')
+    except UserProfile.DoesNotExist:
+        # 프로필 정보가 아예 없는 경우도 프로필 입력 페이지로 리디렉션
+        return redirect('accounts:profile')
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('yolov5_django:index')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/profile.html', context)
